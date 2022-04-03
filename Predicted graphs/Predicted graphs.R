04fca8981f0e2524433cf9239ff516909d7445c2
Caroline Tang <17ct24@queensu.ca>
2022-04-03 15:57:34 -0400

    fixed predicted graphs

--- Predicted graphs/Predicted graphs.R
+++ Predicted graphs/Predicted graphs.R
@@ -7,26 +7,26 @@ library(tidyverse)
 # Boxplot of transmission vs mutation rate ---------------
 # Create fake data on transmission and mutation rates
 set.seed(1)
-nullViruses <- data.frame(group = rep("null", 50),
+nullViruses <- data.frame(group = rep("H0", 50),
                           transmission = rep(c("aerosolic", "vector", "body fluids", "fecal-oral", "blood/sexual"), 10),
                           mutation = c(rnorm(50, 0.01, 0.001)))
 
 # Create fake data with one method higher than the rest 
-biasedViruses <- data.frame(group = rep("bias", 50),
+biasedViruses <- data.frame(group = rep("HA", 50),
                             transmission = rep(c("aerosolic", "vector", "body fluids", "fecal-oral", "blood/sexual"), 10),
                             mutation = c(rnorm(50, 0.01, 0.001)))
 biasedViruses$mutation[biasedViruses$transmission == "vector"] <- biasedViruses$mutation[biasedViruses$transmission == "vector"] * 1.1
 
 #Bind two groups together
 viruses <- rbind(nullViruses, biasedViruses)
-viruses$group <- factor(viruses$group, levels = c("null", "bias"))
+viruses$group <- factor(viruses$group, levels = c("H0", "HA"))
 
 # Create boxplot
-png("Predicted graphs/transmission-vs-mutation.png", width = 5, height = 4, units = "in", res = 300)
-ggplot(data = viruses, aes(x = transmission, y = mutation)) +
+png("Predicted graphs/transmission-vs-mutation.png", width = 6, height = 4, units = "in", res = 300)
+ggplot(data = viruses, aes(x = transmission, y = mutation, fill = transmission)) +
   geom_boxplot() +
   theme_bw() +
-  labs(x = "Transmission method", y = "Mutation Rate") +
+  labs(x = "Transmission method", y = "Mutation rate") +
   facet_grid(rows = vars(group))
 dev.off()
 
